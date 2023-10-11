2d240e399d7936c894e5b406ce416de99ba2b3cb
phpusr <phpusr@gmail.com>
2023-10-11 14:14:54 +0500

    Added checking errors

--- main.py
+++ main.py
@@ -27,25 +27,40 @@ class DocxImageExtractorApp:
 
         root.mainloop()
 
-    def select_dir_callback(self):
+    def select_dir_callback(self) -> None:
         self.file_path_text.delete(0, END)
         self.file_path_text.insert(0, filedialog.askdirectory(title='Выберите папку с DOCX-файлами'))
 
-    def extract_images_callback(self):
+    def extract_images_callback(self) -> None:
         docx_files_path = Path(self.file_path_text.get())
-        self.extract_images(docx_files_path)
-        messagebox.showinfo('', 'Изображения из файлов извлечены')
+
+        if not docx_files_path.is_dir():
+            messagebox.showwarning('Внимание!', f'Папка "{docx_files_path}" не существует')
+            return
+
+        file_count = self.extract_images(docx_files_path)
+
+        if file_count == 0:
+            messagebox.showwarning('Внимание!', f'DOCX-файлы не обнаружены в папке: "{docx_files_path}"')
+            return
+
+        messagebox.showinfo('Готово', f'Изображения из ({file_count}) файлов извлечены')
 
     @staticmethod
-    def extract_images(dir_path: Path):
+    def extract_images(dir_path: Path) -> int:
         print('dir_path:', dir_path)
+
+        file_count = 0
 
         for file_path in dir_path.iterdir():
             if file_path.is_file() and file_path.suffix == '.docx':
+                file_count += 1
                 print('- Extracting images from:', file_path)
                 images_path = dir_path / (file_path.name + '_images')
                 images_path.mkdir(exist_ok=True)
                 docx2txt.process(file_path, images_path)
+
+        return file_count
 
 
 if __name__ == '__main__':
